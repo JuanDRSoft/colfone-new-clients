e8ed71dde1b38d63a0289ca51c4f3e5a3cc04304
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2024-01-02 21:49:43 -0500

    feat/ youtube info

--- src/pages/AddOrder.jsx
+++ src/pages/AddOrder.jsx
@@ -3,6 +3,7 @@ import { plataforms, servicesEx } from "../utils/data";
 import useAuth from "../hooks/useAuth";
 import { formatearNumero } from "../utils/api";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const AddOrder = () => {
   const [plataformSelect, setPlataform] = useState("");
@@ -10,8 +11,11 @@ const AddOrder = () => {
   const [productSelect, setProduct] = useState("");
   const [tab, setTab] = useState(0);
 
-  const [dataServices, setDataServices] = useState(servicesEx);
+  const [dataServices, setDataServices] = useState([]);
   const [dataProducts, setDataProducts] = useState([]);
+
+  const [videoData, setVideoData] = useState(null);
+  const [videoUrl, setVideoUrl] = useState("");
 
   const { services, products, wallet } = useAuth();
 
@@ -42,10 +46,44 @@ const AddOrder = () => {
     setTab(tab - 1);
   };
 
+  useEffect(() => {
+    const getVideoIdFromUrl = (url) => {
+      const regex = /[?&]v=([^?&]+)/;
+      const match = url.match(regex);
+      return match ? match[1] : null;
+    };
+
+    // Reemplaza 'TU_CLAVE_DE_API' con tu clave de API
+    const apiKey = "AIzaSyC7XUuqDaZaEVzTQt1D3A9d0VBjdSVZSd4";
+
+    // Reemplaza 'URL_DEL_VIDEO' con la URL real del video que deseas obtener
+
+    const videoId = getVideoIdFromUrl(videoUrl);
+
+    if (videoId) {
+      const fetchData = async () => {
+        try {
+          // Realiza la solicitud a la API de YouTube
+          const response = await axios.get(
+            `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${videoId}&key=${apiKey}`
+          );
+
+          // Actualiza el estado con los datos del video
+          setVideoData(response.data.items[0]);
+        } catch (error) {
+          console.error("Error al obtener datos del video:", error);
+        }
+      };
+
+      fetchData();
+    }
+  }, [videoUrl]);
+
   return (
     <div
       className={`bg-[#26A699] p-5 ${
-        dataProducts.length < 8 && tab == 1 && "h-screen"
+        (dataProducts.length < 8 && tab == 1 && "h-screen") ||
+        (dataServices.length < 1 && "h-screen")
       }`}
     >
       {tab == 0 && (
@@ -75,26 +113,28 @@ const AddOrder = () => {
             </div>
           </div>
 
-          <div className="bg-[#1E3050] mt-5 rounded-lg p-2">
-            <h1 className="text-white text-center font-semibold text-lg">
-              2. Seleccionar servicio
-            </h1>
+          {dataServices.length > 0 && (
+            <div className="bg-[#1E3050] mt-5 rounded-lg p-2">
+              <h1 className="text-white text-center font-semibold text-lg">
+                2. Seleccionar servicio
+              </h1>
 
-            <div className="grid  gap-3 p-4">
-              {dataServices.map((e) => (
-                <div
-                  onClick={() => onSelectService(e)}
-                  className={`${
-                    serviceSelect == e.name ? "bg-[#F29627]" : "bg-white"
-                  }  p-2 rounded-lg`}
-                >
-                  <p className="text-center font-bold text-lg text-[#1E3050] hover:text-white">
-                    {e.name}
-                  </p>
-                </div>
-              ))}
+              <div className="grid  gap-3 p-4">
+                {dataServices.map((e) => (
+                  <div
+                    onClick={() => onSelectService(e)}
+                    className={`${
+                      serviceSelect == e.name ? "bg-[#F29627]" : "bg-white"
+                    }  p-2 rounded-lg`}
+                  >
+                    <p className="text-center font-bold text-lg text-[#1E3050] hover:text-white">
+                      {e.name}
+                    </p>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
+          )}
         </>
       )}
 
@@ -136,35 +176,136 @@ const AddOrder = () => {
         </>
       )}
 
-      <div className="mt-10 grid grid-cols-2 gap-5">
-        {tab == 0 && (
-          <Link
-            to="/dashboard"
-            className="text-[#1E3050] gap-2 flex justify-center items-center bg-white p-1 rounded-lg text-lg font-bold"
-          >
-            Cerrar
-            <i class="far fa-times-circle"></i>
-          </Link>
-        )}
+      {tab == 2 && plataformSelect == "650f6df8a9f769232a5d6c8c" && (
+        <>
+          <div className="bg-[#1E3050] mt-5 rounded-lg p-2">
+            <h1 className="text-white text-center font-semibold text-lg">
+              Ingresa el link del video
+            </h1>
 
-        {tab >= 1 && (
+            <div className="px-5 mt-2">
+              <input
+                className="bg-white w-full rounded-full mb-3 p-1 pl-3"
+                value={videoUrl}
+                onChange={(e) => setVideoUrl(e.target.value)}
+              />
+
+              <div className="rounded-xl overflow-hidden">
+                <img
+                  src={videoData?.snippet.thumbnails.medium.url}
+                  width={videoData?.snippet.thumbnails.medium.width}
+                  height={videoData?.snippet.thumbnails.medium.height}
+                />
+              </div>
+
+              <h1 className="text-white font-semibold text-sm text-center mt-1">
+                {videoData?.snippet.title}
+              </h1>
+
+              <div className="grid mt-5 gap-3 mb-5">
+                <div className="bg-orange-400 rounded-xl p-1">
+                  <p className="text-white text-center font-semibold">
+                    Vistas:{" "}
+                    {videoData
+                      ? formatearNumero(videoData?.statistics.viewCount)
+                      : "N/D"}
+                  </p>
+                </div>
+
+                <div className="bg-orange-400 rounded-xl p-1">
+                  <p className="text-white text-center font-semibold">
+                    Likes:{" "}
+                    {videoData
+                      ? formatearNumero(videoData?.statistics.likeCount)
+                      : "N/D"}
+                  </p>
+                </div>
+
+                <div className="bg-orange-400 rounded-xl p-1">
+                  <p className="text-white text-center font-semibold">
+                    Comentarios:{" "}
+                    {videoData
+                      ? formatearNumero(videoData?.statistics.commentCount)
+                      : "N/D"}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+
+      {tab == 2 && (
+        <>
+          <div className="bg-[#1E3050] mt-5 rounded-lg p-2 px-5">
+            <h1 className="text-white text-center font-semibold text-lg">
+              Servicio a pagar
+            </h1>
+            <div className="bg-white p-2 rounded-lg py-5 mt-2 mb-4">
+              <h1 className="text-[#1E3050] text-center font-semibold text-lg">
+                {formatearNumero(productSelect.cantidad)} {serviceSelect}
+              </h1>
+
+              <p className="text-center font-bold text-[#1E3050] text-4xl">
+                $ {formatearNumero(productSelect[wallet.currency].toFixed(0))}{" "}
+                {wallet.currency}
+              </p>
+            </div>
+          </div>
+
+          <div className="grid mt-5 gap-3">
+            <button className="text-white gap-2 flex justify-center items-center bg-[#1E3050] p-1 rounded-lg text-lg font-bold">
+              Pagar Ahora
+              <i class="fas fa-arrow-circle-right"></i>
+            </button>
+
+            <button className="text-[#1E3050] gap-2 flex justify-center items-center bg-white p-1 rounded-lg text-lg font-bold">
+              Enviar al carrito
+              <i class="fas fa-cart-plus"></i>
+            </button>
+
+            <button
+              onClick={back}
+              className="text-[#1E3050] gap-2 flex justify-center items-center bg-white p-1 rounded-lg text-lg font-bold"
+            >
+              <i class="fas fa-arrow-circle-left"></i>
+              Atras
+            </button>
+          </div>
+        </>
+      )}
+
+      {tab !== 2 && (
+        <div className="mt-10 grid grid-cols-2 gap-5">
+          {tab == 0 && (
+            <Link
+              to="/dashboard"
+              className="text-[#1E3050] gap-2 flex justify-center items-center bg-white p-1 rounded-lg text-lg font-bold"
+            >
+              Cerrar
+              <i class="far fa-times-circle"></i>
+            </Link>
+          )}
+
+          {tab >= 1 && (
+            <button
+              onClick={back}
+              className="text-[#1E3050] gap-2 flex justify-center items-center bg-white p-1 rounded-lg text-lg font-bold"
+            >
+              <i class="fas fa-arrow-circle-left"></i>
+              Atras
+            </button>
+          )}
+
           <button
-            onClick={back}
-            className="text-[#1E3050] gap-2 flex justify-center items-center bg-white p-1 rounded-lg text-lg font-bold"
+            onClick={() => setTab(tab + 1)}
+            className="bg-[#1E3050] rounded-lg flex justify-center items-center gap-2 text-white p-1 font-bold"
           >
-            <i class="fas fa-arrow-circle-left"></i>
-            Atras
+            Siguiente
+            <i class="fas fa-arrow-circle-right"></i>
           </button>
-        )}
-
-        <button
-          onClick={() => setTab(tab + 1)}
-          className="bg-[#1E3050] rounded-lg flex justify-center items-center gap-2 text-white p-1 font-bold"
-        >
-          Siguiente
-          <i class="fas fa-arrow-circle-right"></i>
-        </button>
-      </div>
+        </div>
+      )}
     </div>
   );
 };
