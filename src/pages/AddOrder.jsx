8138b66964e00d326114f77d71b73cd2dbde246e
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2024-01-02 22:31:40 -0500

    fix/ info video

--- src/pages/AddOrder.jsx
+++ src/pages/AddOrder.jsx
@@ -48,7 +48,8 @@ const AddOrder = () => {
 
   useEffect(() => {
     const getVideoIdFromUrl = (url) => {
-      const regex = /[?&]v=([^?&]+)/;
+      const regex =
+        /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/i;
       const match = url.match(regex);
       return match ? match[1] : null;
     };
